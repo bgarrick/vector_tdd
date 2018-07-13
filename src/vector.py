254ab114ba73161ecf51bd03c466907f21b1a90b
Bessie Garrick <bessie.garrick@mckesson.com>
2018-07-13 09:53:04 -0500

    scale

--- src/vector.py
+++ src/vector.py
@@ -1,3 +1,17 @@
 class Vector:
     def __init__(self, nums):
         self.nums = nums
+        self.dims = len(nums)
+        self.norm = self._length()
+
+    def __eq__(self, other):
+        return self.nums == other.nums
+
+    def _length(self):
+        return sum([i**2 for i in self.nums]) ** 0.5
+
+    def unit_vector(self):
+        return Vector([i/self.norm for i in self.nums])
+    
+    def scale(self, s):
+        return Vector([i*s for i in self.nums])
